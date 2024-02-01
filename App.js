805b9b871cf426c0cc5250cfeaf58ae3bdcde0d0
Marti1213 <141115203+Marti1213@users.noreply.github.com>
2024-02-01 11:01:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import QRCodeGenerator from './QRCodeGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGenerator />
+    </div>
+  );
+}
+
+export default App;
